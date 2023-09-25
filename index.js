9e01d9f35879ef91cba6f77b527692202cfbf5a4
alejandroFMA <alexfmarquez@gmail.com>
2023-09-25 20:48:46 +0200

    faltan dos ejercicios

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function ordenarArray(arr){
 
  function obtenerPares(a){
     let numPares=[]
-    for (let i = 0; index < a.length; i++) {
+    for (let i = 0; i < a.length; i++) {
         if (a[i] % 2 == 0) {
             numPares.push(a[i])
         }
@@ -65,5 +65,104 @@ function ordenarArray(arr){
 
   }
 
- ////// hasta aqui bien
-  
+
+function pintarArray(arr) {
+    
+}
+
+
+
+function arrayMapi(arr) {
+    
+}
+
+
+function eliminarDuplicados(arr){
+
+    return [...new Set(arr)]; //[...new Set] creaba nuevo array sin dupes
+}
+
+let arrayNumerosNeg = [0, -1, -2, -3, -4, -5, -6, -7, -8, -9];
+let holaMundo = ['Hola', 'Mundo'];
+let loGuardoTodo = [ 'hola', 'que', 23, 42.33, 'tal'];
+let arrayDeArrays =[[756, 'nombre'], [225, 'apellido'], [298, 'direccion']];
+
+function multiplicacion(a, b){
+
+return a*b;
+}
+
+function division(a, b){
+
+    return a/b;
+}
+
+function esPar(a){
+    if(a % 2 == 0){
+
+        return true  
+    }
+    
+
+    return false
+}
+
+
+function resta(a,b){
+
+    return a-b;
+}
+ 
+let arrayFunciones = [suma, resta, multiplicacion];
+
+for (let i = 0; i < arrayFunciones.length; i++) {
+    const element = array[index];
+    
+}
+
+
+
+function ordenarArray2(arr){
+
+    let newArr= arr.slice();
+
+    return newArr.sort((a,b)=> b-a)
+}
+
+
+function obtenerImpares(a){
+    
+    let arrayImp=[];
+
+    for(let i = 0; i < a.length; i++)
+        if(a[i] % 2 !== 0){
+        arrayImp.push(a[i])
+    }
+    return arrayImp
+
+}
+
+
+function sumarArray(a){
+
+let suma = 0;
+   
+    for (let i = 0; i < a.length; i++) {
+      
+        suma += a[i]
+        
+    }
+return suma;
+}
+
+function multiplicarArray(a) {
+
+    let multiplicar = 1; //ojo!!!! multiplicar y dividir por 0 no
+
+    for (let i = 0; i < a.length; i++) {
+
+        multiplicar *= a[i];
+
+    }
+    return multiplicar;
+}
